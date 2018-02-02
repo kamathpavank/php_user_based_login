9c779f47ec13cf4737413284e9e9d48355c037b1
Pavankumar Kamath <pavan.g.kamath@gmail.com>
2018-02-02 18:24:53 +0530

    Update connection.php
    postgres code for connection

--- connection.php
+++ connection.php
@@ -5,6 +5,11 @@ $dbname = 'login';
 $password = 'abcd@1234';
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
+
+//Postgres code
+//$conn = pg_connect("host=localhost dbname=login user=root password=abcd@1234");
+
+
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
