b2eafe8603ca84474bab7c3457b1714c69d295f9
pavankamath1406 <pavan.g.kamath@gmail.com>
2018-03-02 15:09:35 +0530

    Created two different table for authentication and access

--- validate.php
+++ validate.php
@@ -12,45 +12,37 @@ session_start();
 	$username = $_POST["username"];
 	$password = $_POST["password"];
 
-	$sql = "SELECT * FROM `userbased` WHERE `username` = '$username' AND `password` = '$password'";
+	$sql = "SELECT * FROM `auth` WHERE `username` = '$username' AND `password` = '$password'";
 	$result = $conn->query($sql);
 	
-	//postgres code of querying data
-	//$result = pg_query($conn, "SELECT * FROM `userbased` WHERE `username` = '$username' AND `password` = '$password'");
-
+	$sql2  = "SELECT * FROM `access` WHERE `username` = '$username'";
+	$result2 =  $conn->query($sql2);
+	
 
 	if($result->num_rows > 0){
-		
-	//potgres sql command
-	//if (pg_result_status($result) > 0) {
-		
+
+		$access_levels = array();
 		$row = $result->fetch_assoc();
-		
-		//postgres code for fetch_assoc()
-		//$row = pg_fetch_assoc($result);
-		
-		$test = $row['access'];
-		$_SESSION["access"] = $test;
 		$_SESSION["username"]=$row['username'];
-		//display first page as imac if access level contains 1
-		if($test == 1 || $test == 12 || $test == 13){
-			header('Location: imac.php');
+	
+		//extract all access level of user in array
+
+		while($row2 = $result2->fetch_assoc()){
+			array_push($access_levels,$row2['access_level']);		
 		}
 
-		//display first page as ilab if access level contains 2
-		else if($test == 2 || $test == 23){
-			header('Location: ilab.php');
+		if(in_array(3,$access_levels)){
+			header('Location:robotics.php');
 		}
 
-		//display first page as robotics if access level contains 3
-		else if($test == 3){
-			header('Location: robotics.php');
+		if(in_array(1,$access_levels)){
+			header('Location:imac.php');
 		}
 
-		//Admin
-		else{
-			header('Location: admin.php');
+		if(in_array(2,$access_levels)){
+			header('Location:ilab.php');
 		}
+		
 	}
 
 	else{
