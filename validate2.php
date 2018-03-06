1c641732071714560f4dc69bd5e274ea7825bf52
Pavankumar Kamath <pavan.g.kamath@gmail.com>
2018-03-06 16:08:12 +0530

    Update validate2.php

--- validate2.php
+++ validate2.php
@@ -1,88 +1,74 @@
 <?php
-// Start the session
+	//include database config file 
+include('login_connect.php');
+//require_once('interview2017Connect.php');
 session_start();
-?>
-
-<?php
-	require 'connection.php';
-?>
-
-<?php
-
-	$username = $_POST["username"];
-	$password = $_POST["password"];
-
-	$sql = "SELECT * FROM `userbased` WHERE `username` = '$username' AND `password` = '$password'";
-	$result = $conn->query($sql);
+$id="0";
+$console='';
+$session_id='';
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+	$id=$_POST["loginID"];
+	$password=$_POST["loginPassword"];
+	// Checking the received data for correct expressions:
+	if (!preg_match("/^[a-zA-Z]*$/",$id)){
+		echo "Enter only characters";
+	}else{
+		//----- PostGRE SQL Commands -----
 	
-	//postgres code of querying data
-	//$result = pg_query($conn, "SELECT * FROM `userbased` WHERE `username` = '$username' AND `password` = '$password'");
-
-
-	if($result->num_rows > 0){
-		
-	//potgres sql command
-	//if (pg_result_status($result) > 0) {
-		
-		$row = $result->fetch_assoc();
-		
-		//postgres code for fetch_assoc()
-		//$row = pg_fetch_assoc($result);
-		
-		//access value in test
-		$test = $row['access'];
-		$_SESSION["access"] = $test;
-		$_SESSION["username"]=$row['username'];
-		//display first page as overview if access level contains 1
-		if($test == 1){
-			header('Location: overview.php');
-		}
-
-		//display first page as codestrike if access level contains 2
-		else if($test == 2){
-			header('Location: codestrike.php');
-		}
-
-		//Admin
-		else{
-			header('Location: overview.php');
-		}
-	}
-
-	else{
-		$_SESSION["credentials"] = "Invalid username and password";
-		header('Location: index.php');
-	}
-
-	if ((pg_result_status($result)==2) && (pg_num_rows($result)==1) ) {
+		//creating Query:
+		$result = pg_query_params($dbconn, "SELECT * FROM $tableName WHERE username=$1", array($id));
+		// $result = pg_query($dbconn, "SELECT * FROM $tableName WHERE username='$id'");
+		//Checking for Success Result and only ONE Row entry:
+		if ((pg_result_status($result)==2) && (pg_num_rows($result)==1) ) {
 			$row = pg_fetch_assoc($result);
 			$hash = $row['password'];
-
+			//Take access value
+			$test = $row['access'];
 			//Verifying the password:
 			if (password_verify($password,$hash)){ // <--Replace here
 				// Seting Session variables:
 				$_SESSION['userId']=$row['uniqueID'];
 				$_SESSION['username']=$row['username'];
-				//access value in test
-				$_SESSION["access"] = $test;
-				//display first page as overview if access level contains 1
-			if($test == 1){
+				$_SESSION['access']=$test;
+				if($test == 1){
 				header('Location: overview.php');
-			}
+				}
 
-			//display first page as codestrike if access level contains 2
-			else if($test == 2){
-				header('Location: codestrike.php');
-			}
+				//display first page as codestrike if access level contains 2
+				else if($test == 2){
+					header('Location: events.php');
+				}
 
-			//Admin
+				//Admin
+				else{
+					header('Location: overview.php');
+				}
+			}
 			else{
-				header('Location: overview.php');
+				// Redirecting after unsuccessful login:
+				header("Location:login.php");
 			}
-	}
-	else{
-			// Redirecting after unsuccessful login:
-			header("Location:login.php");
 		}
-		
+		// -X-X-X- End of PostGRE SQL Commands -X-X-X-
+		//----- SQL Commands -----
+		//create query
+		// $query="SELECT * FROM $tableName WHERE username='$id'";
+		// $result = mysqli_query($conn,$query);
+		//check the query in the database
+		/*
+		if ($result){
+			$row=mysqli_fetch_assoc($result);
+			$hash=$row["password"];
+			if (password_verify($password,$hash)){
+				$_SESSION['userId']=$row['Unique ID'];
+				$_SESSION['username']=$row['username'];
+				header('Location:index.php');
+			}else{
+				header('Location:login.php');
+			}
+		}
+		*/
+		// -X-X-X- End of SQL Commands -X-X-X-
+	}
+}
 ?>
